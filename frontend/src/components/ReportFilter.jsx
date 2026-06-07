5551dcde6f18185ab5d749cad35023b2d0dfb5d6
Mizan <comanfake@gmail.com>
2026-06-07 22:52:56 +0600

    Something Updated

--- frontend/src/components/ReportFilter.jsx
+++ frontend/src/components/ReportFilter.jsx
@@ -17,19 +17,19 @@ const ReportFilter = ({
 }) => {
   return (
     <div className="bg-gray-900 p-4 rounded-2xl mb-6 space-y-3 border border-gray-700">
-      <div className="grid grid-cols-2 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
         <input
           type="date"
           value={fromDate}
           onChange={(e) => setFromDate(e.target.value)}
-          className="p-3 bg-gray-800 rounded-lg outline-none"
+          className="w-full p-3 bg-gray-800 rounded-lg outline-none"
         />
 
         <input
           type="date"
           value={toDate}
           onChange={(e) => setToDate(e.target.value)}
-          className="p-3 bg-gray-800 rounded-lg outline-none"
+          className="w-full p-3 bg-gray-800 rounded-lg outline-none"
         />
       </div>
 
@@ -48,7 +48,7 @@ const ReportFilter = ({
             ))}
         </select>
       </div>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-3">
         <button
           onClick={handleView}
           className="bg-blue-600 hover:bg-blue-700 p-3 rounded-lg font-semibold"
