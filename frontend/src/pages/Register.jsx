fcfd320eedd699ad7e2e96d2b2ca970ae8abab21
Mizan <comanfake@gmail.com>
2026-06-08 02:19:49 +0600

    Something Updated

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -57,7 +57,7 @@ const Register = () => {
 
   if (!accessGranted) {
     return (
-      <div className="flex items-start justify-center h-screen bg-gray-50 pt-10 sm:pt-16 md:pt-18 lg:pt-20">
+      <div className="flex items-start justify-center h-screen bg-gray-50 pt-14 sm:pt-20 md:pt-22 lg:pt-24">
         <form
           onSubmit={handleAccessSubmit}
           className="bg-white p-6 rounded-xl shadow-lg w-80 text-center"
