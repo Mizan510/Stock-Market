cd406cb8fa9a23d04542350adb7c24231c0023db
Mizan <comanfake@gmail.com>
2026-06-26 22:57:15 +0600

    Updated

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,15 +1,27 @@
-const mongoose = require("mongoose");
+// backend/models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: String,
-    email: { type: String, unique: true },
-    password: String,
-    isActive: { type: Boolean, default: true },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
   },
-);
+  password: {
+    type: String,
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false, // CHANGE THIS FROM true TO false
+  },
+}, {
+  timestamps: true,
+});
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
